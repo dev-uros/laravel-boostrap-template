6333a8aee5746cbf517e6965f125c38f04a1caf4
dev-uros <uros.minic@kentkart.rs>
2025-09-17 00:14:18 +0200

    Adding seed user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,11 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'surname'=>'Testivojevic',
             'email' => 'test@example.com',
+            'password' => Hash::make('Password1234!'),
+            'is_admin'=>true,
+            'is_active' => true
         ]);
     }
 }
